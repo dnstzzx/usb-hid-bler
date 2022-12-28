a3a504dff35fd5941ca2b8d27d12046a141281ad
dnstzzx <dnstzzx@qq.com>
2022-12-29 01:12:47 +0800

    add: c3_hid1 board

---
+++ main/boards/board_c3_hid1.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include "board_common.h"
+
+#define BOARD_HAS_LED
+#define BOARD_PIN_LED   (7)   
+
+/*
+#define BOARD_HAS_FINGERPRINT_MODULE
+#define BOARD_FINGERPRINT_PIN_EN
+#define BOARD_FINGERPRINT_PIN_TX
+#define BOARD_FINGERPRINT_PIN_RX
+#define BOARD_FINGERPRINT_PIN_IRQ
+
+#define BOARD_HAS_BATTARY
+#define BOARD_PIN_BATTARY   (2)
+#define BOARD_BATTARY_VOLTAGE_SCALE (2)
+*/
+
+#define BOARD_USB1_PIN_DP   (3)
+#define BOARD_USB1_PIN_DM   (4)
+#define BOARD_USB2_PIN_DP   (5)
+#define BOARD_USB2_PIN_DM   (6)
